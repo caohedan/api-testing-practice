327a9bd03b1d17a8801d4dce8a87c0898a1cf73b
caohedan <m18390990364@163.com>
2018-07-30 12:32:34 +0800

    '[amelia]checkCircuitsForDriver'

--- src/test/java/exercises/RestAssuredExercises2Test.java
+++ src/test/java/exercises/RestAssuredExercises2Test.java
@@ -4,8 +4,14 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 
 public class RestAssuredExercises2Test {
@@ -21,19 +27,35 @@ public class RestAssuredExercises2Test {
 			setBasePath("/api/f1").
 			build();
 	}
-	
+    static Stream<Arguments> driverDataProvider() {
+        return Stream.of(
+                Arguments.of("monza", "Italy")
+        );
+    }
 	/*******************************************************
 	 * Use junit-jupiter-params for @ParameterizedTest that
 	 * specifies in which country
-	 * a specific circuit can be found (specify that Monza 
-	 * is in Italy, for example) 
+	 * a specific circuit can be found (specify that Monza
+	 * is in Italy, for example)
 	 ******************************************************/
 
 	//todo
+    @ParameterizedTest
+    @MethodSource("driverDataProvider")
+    public void checkCircuitsForDriver(String driverName, String permanentNumber) {
 
+        given().
+                spec(requestSpec).
+                pathParam("driver", driverName).
+                when().
+                get("/circuits/{driver}.json").
+                then().
+                assertThat().
+                body("MRData.CircuitTable.Circuits[0].Location.country", equalTo(permanentNumber));
+    }
 	/*******************************************************
 	 * Use junit-jupiter-params for @ParameterizedTest that specifies for all races
-	 * (adding the first four suffices) in 2015 how many  
+	 * (adding the first four suffices) in 2015 how many
 	 * pit stops Max Verstappen made
 	 * (race 1 = 1 pitstop, 2 = 3, 3 = 2, 4 = 2)
 	 ******************************************************/
@@ -41,30 +63,30 @@ public class RestAssuredExercises2Test {
 	//todo
 
 	/*******************************************************
-	 * Request data for a specific circuit (for Monza this 
+	 * Request data for a specific circuit (for Monza this
 	 * is /circuits/monza.json)
 	 * and check the country this circuit can be found in
 	 ******************************************************/
-	
+
 	@Test
 	public void checkCountryForCircuit() {
-		
+
 		given().
 			spec(requestSpec).
 		when().
 		then();
 	}
-	
+
 	/*******************************************************
 	 * Request the pitstop data for the first four races in
 	 * 2015 for Max Verstappen (for race 1 this is
 	 * /2015/1/drivers/max_verstappen/pitstops.json)
 	 * and verify the number of pit stops made
 	 ******************************************************/
-	
+
 	@Test
 	public void checkNumberOfPitstopsForMaxVerstappenIn2015() {
-		
+
 		given().
 			spec(requestSpec).
 		when().
