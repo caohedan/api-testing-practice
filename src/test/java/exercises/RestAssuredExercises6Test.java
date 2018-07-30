0eb369b049c492a3deb033a85b1b682250661776
caohedan <m18390990364@163.com>
2018-07-30 18:25:42 +0800

    '[amelia]test6-2'

--- src/test/java/exercises/RestAssuredExercises6Test.java
+++ src/test/java/exercises/RestAssuredExercises6Test.java
@@ -1,5 +1,6 @@
 package exercises;
 
+
 import dataentities.Car;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
@@ -7,61 +8,71 @@ import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.core.Is.is;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class RestAssuredExercises6Test {
 
-	private static RequestSpecification requestSpec;
+    private static RequestSpecification requestSpec;
 
 
-	@BeforeAll
-	static void setUp() {
+    @BeforeAll
+    static void setUp() {
 
-		requestSpec = new RequestSpecBuilder().
-				setBaseUri("http://localhost").
-				setPort(9876).
-				setContentType(ContentType.JSON).
-				build();
-	}
+        requestSpec = new RequestSpecBuilder().
+                setBaseUri("http://localhost").
+                setPort(9876).
+                setContentType(ContentType.JSON).
+                build();
+    }
 
 
-	/*******************************************************
-	 * Create a new Car object that represents a 2012 Ford Focus
-	 * POST this object to /cars/postcar
-	 * Verify that the response HTTP status code is equal to 200
-	 ******************************************************/
+    /*******************************************************
+     * Create a new Car object that represents a 2012 Ford Focus
+     * POST this object to /cars/postcar
+     * Verify that the response HTTP status code is equal to 200
+     ******************************************************/
 
-	@Test
-	public void checkThatPostingA2012FordFocusReturnsHttp200() {
+    @Test
+    public void checkThatPostingA2012FordFocusReturnsHttp200() {
 
-		Car car = new Car("Ford","Focus",2012);
+        Car car = new Car("Ford", "Focus", 2012);
 
-		given().
-				spec(requestSpec).
-				body(car).
-				when().
-				log().all().
-				post("/car/postcar").
-				then().
-				assertThat().
-				statusCode(200);
-	}
+        given().
+                spec(requestSpec).
+                body(car).
+                when().
+                log().all().
+                post("/car/postcar").
+                then().
+                assertThat().
+                statusCode(200);
+    }
 
-	/*******************************************************
-	 * Perform a GET to /cars/getcar/alfaromeogiulia
-	 * Store the response in a Car object
-	 * Verify, using that object, that the model year = 2016
-	 * Use the standard Assert.assertEquals(expected,actual)
-	 * as provided by JUnit for the assertion
-	 ******************************************************/
+    /*******************************************************
+     * Perform a GET to /cars/getcar/alfaromeogiulia
+     * Store the response in a Car object
+     * Verify, using that object, that the model year = 2016
+     * Use the standard Assert.assertEquals(expected,actual)
+     * as provided by JUnit for the assertion
+     ******************************************************/
 
-	@Test
-	public void checkThatRetrievingAnAlfaRomeoGiuliaShowsModelYear2016() {
+    @Test
+    public void checkThatRetrievingAnAlfaRomeoGiuliaShowsModelYear2016() {
 
-		given().
-			spec(requestSpec).
-		when();
+        given().
+                spec(requestSpec).
+                when();
+        Car car =
+                given().
+                        spec(requestSpec).
+                        when().
+                        log().all().
+                        get("/car/getcar/alfaromeogiulia").
+                        as(Car.class);
 
-		// Put your assert here
-	}
+        assertThat(car.getYear(), is(2016));
+        // Put your assert here
+    }
 }
